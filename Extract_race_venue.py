58ff2b5059368b8dc9e8bb364d38c57198c88880
Parth Adalja <parthadalja29@gmail.com>
2023-08-15 23:45:27 +0530

    Data extraction of race vanues from 1950-2022

---
+++ Extract_race_venue.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import requests
+import numpy as np
+
+races = {
+    'season': [],
+    'round': [],
+    'circuit_id': [],
+    'lat': [],
+    'long': [],
+    'country': [],
+    'date': [],
+    'url': []
+}
+
+for year in range(1950-2023):
+
+    url = 'https://ergast.com/api/f1/{}.json'
+    r = requests.get(url.format(year))
+    json = r.json()
+
+    for item in json['MRData']['RaceTable']['Races']:
+        try:
+            races['season'].append(int(item['season']))
+        except:
+            races['season'].append(None)
+
+        try:
+            races['round'].append(int(item['round']))
+        except:
+            races['round'].append(None)
+
+        try:
+            races['circuit_id'].append(item['Circuit']['circuitId'])
+        except:
+            races['circuit_id'].append(None)
+
+        try:
+            races['lat'].append(float(item['Circuit']['Location']['lat']))
+        except:
+            races['lat'].append(None)
+
+        try:
+            races['long'].append(float(item['Circuit']['Location']['long']))
+        except:
+            races['long'].append(None)
+
+        try:
+            races['country'].append(item['Circuit']['Location']['country'])
+        except:
+            races['country'].append(None)
+
+        try:
+            races['date'].append(item['date'])
+        except:
+            races['date'].append(None)
+
+        try:
+            races['url'].append(item['url'])
+        except:
+            races['url'].append(None)
+
+races = pd.DataFrame(races)
