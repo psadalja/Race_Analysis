2857c391445cfaf2012c769acd303af2db34a04b
Parth Adalja <parthadalja29@gmail.com>
2023-08-16 18:44:01 +0530

    Updated venue script to return the df

--- Extract_race_venue.py
+++ Extract_race_venue.py
@@ -2,66 +2,81 @@ import pandas as pd
 import requests
 import numpy as np
 
-races = {
-    'season': [],
-    'round': [],
-    'circuit_id': [],
-    'lateral': [],
-    'longitude': [],
-    'country': [],
-    'date': [],
-    'url': []
-}
+# races = {
+#     'season': [],
+#     'round': [],
+#     'circuit_id': [],
+#     'lateral': [],
+#     'longitude': [],
+#     'country': [],
+#     'date': [],
+#     'url': []
+# }
 
-for year in list(range(1950, 2023)):
 
-    url = 'https://ergast.com/api/f1/{}.json'
-    r = requests.get(url.format(year))
-    json = r.json()
-    # print(json)
+def extrct_race_venue():
 
-    for item in json['MRData']['RaceTable']['Races']:
-        try:
-            races['season'].append(int(item['season']))
-        except:
-            races['season'].append(None)
+    races = {
+        'season': [],
+        'round': [],
+        'circuit_id': [],
+        'lateral': [],
+        'longitude': [],
+        'country': [],
+        'date': [],
+        'url': []
+    }
+    for year in list(range(1950, 2023)):
 
-        try:
-            races['round'].append(int(item['round']))
-        except:
-            races['round'].append(None)
+        url = 'https://ergast.com/api/f1/{}.json'
+        r = requests.get(url.format(year))
+        json = r.json()
+        # print(json)
 
-        try:
-            races['circuit_id'].append(item['Circuit']['circuitId'])
-        except:
-            races['circuit_id'].append(None)
+        for item in json['MRData']['RaceTable']['Races']:
+            try:
+                races['season'].append(int(item['season']))
+            except:
+                races['season'].append(None)
 
-        try:
-            races['lateral'].append(float(item['Circuit']['Location']['lat']))
-        except:
-            races['lateral'].append(None)
+            try:
+                races['round'].append(int(item['round']))
+            except:
+                races['round'].append(None)
 
-        try:
-            races['longitude'].append(
-                float(item['Circuit']['Location']['long']))
-        except:
-            races['longitude'].append(None)
+            try:
+                races['circuit_id'].append(item['Circuit']['circuitId'])
+            except:
+                races['circuit_id'].append(None)
 
-        try:
-            races['country'].append(item['Circuit']['Location']['country'])
-        except:
-            races['country'].append(None)
+            try:
+                races['lateral'].append(
+                    float(item['Circuit']['Location']['lat']))
+            except:
+                races['lateral'].append(None)
 
-        try:
-            races['date'].append(item['date'])
-        except:
-            races['date'].append(None)
+            try:
+                races['longitude'].append(
+                    float(item['Circuit']['Location']['long']))
+            except:
+                races['longitude'].append(None)
 
-        try:
-            races['url'].append(item['url'])
-        except:
-            races['url'].append(None)
+            try:
+                races['country'].append(item['Circuit']['Location']['country'])
+            except:
+                races['country'].append(None)
 
-races = pd.DataFrame(races)
-print(races.shape)
-races.to_csv('races.csv', index=False)
+            try:
+                races['date'].append(item['date'])
+            except:
+                races['date'].append(None)
+
+            try:
+                races['url'].append(item['url'])
+            except:
+                races['url'].append(None)
+
+    races = pd.DataFrame(races)
+    print(races.shape)
+    races.to_csv('races.csv', index=False)
+    return races
